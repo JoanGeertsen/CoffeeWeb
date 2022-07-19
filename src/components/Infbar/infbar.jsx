c6d6fe9d9c9020c553a77c1ef841eb66837545ff
Camila Arena <56278138+CamilaArena@users.noreply.github.com>
2022-07-19 18:17:49 -0300

    logo facebook

--- src/components/Infbar/infbar.jsx
+++ src/components/Infbar/infbar.jsx
@@ -3,8 +3,8 @@ import "./infbar.css";
 
 function Infbar() {
   return (
-    <nav className="Infbar">               
-        <img src="...public/Imgs/facebook.png" alt="" className="icon" />       
+    <nav className="Infbar">
+      <img src={process.env.PUBLIC_URL + `/Imgs/facebook.png` } alt="" className="icon"/>                     
         <a href="" className="fb">FB icon</a>        
         <a href="" className="fb">INF TXT</a>        
     </nav>
